fbca32ea97fe57ffcfec0b1e01a1f68254798a29
shaisthajabeen610 <67311615+shaisthajabeen610@users.noreply.github.com>
2021-01-06 23:30:30 +0530

    Update Sample_Abstract.java

--- Sample_Abstract.java
+++ Sample_Abstract.java
@@ -1,3 +1,17 @@
-
-public class Sample_Abstract {
+abstract class college{
+    abstract int students();
+    abstract void cash();
+}
+ public class Sample_Abstract extends college{
+    int students(int x,int y){
+        return x;
+    }
+    void cash(){
+        System.out.print("every day a huge amount transaction is done");
+    }
+    public  void main(String[] args) {
+        Sample_Abstract obj1=new Sample_Abstract();
+        obj1.students(100,20);
+        obj1.cash();
+    }
 }
